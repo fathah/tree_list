f864ad217c99d04c8dd1cb84342e75a6b6d74dad
Fathah <abdulfathahkodag@gmail.com>
2021-06-11 18:43:22 +0530

    import spelling

--- test/tree_view_test.dart
+++ test/tree_view_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tree_view/tree_list.dart';
+import 'package:tree_list/tree_list.dart';
 
 /// Tree view tests
 void main() {}
